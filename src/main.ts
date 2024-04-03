4f2518a68b80731dd7ebb64d086402311ed76c42
rendyilfat <rendyilfat@gmail.com>
2024-04-03 15:25:01 +0700

    second

--- src/main.ts
+++ src/main.ts
@@ -23,6 +23,9 @@ import '@ionic/vue/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
+/* Theme Global CSS */
+import './theme/global.css';
+
 const app = createApp(App)
   .use(IonicVue)
   .use(router);
--- src/views/pages/Login.vue
+++ src/views/pages/Login.vue
@@ -12,12 +12,27 @@
                 </ion-toolbar>
             </ion-header>
 
+            <ion-card>
+                <ion-card-header>
+                    <ion-card-title class="text-center">Login</ion-card-title>
+                </ion-card-header>
+                <ion-card-content>
+                    <ion-input label="Email" label-placement="floating" fill="solid" 
+                        placeholder="Masukkan Email"></ion-input>
+                    <ion-input label="Password" label-placement="floating" fill="solid" 
+                        placeholder="Masukkan Password"></ion-input>
 
-
+                    <div class="text-center">
+                        <ion-button size="default">Masuk</ion-button>
+                    </div>
+                </ion-card-content>
+            </ion-card>
 
         </ion-content>
     </ion-page>
 </template>
 
 <script setup lang="ts">
+
 </script>
+<style></style>
